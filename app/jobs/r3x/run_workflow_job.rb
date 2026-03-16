f9727b444dc91337522888f6655bc5e93cee2475
zewelor <zewelor@gmail.com>
2026-03-16 17:11:56 +0000

    Require triggered_by in WorkflowContext
    - Enforce presence of `triggered_by` in `lib/r3x/workflow_context.rb`
    - Make initializer require `triggered_by` and raise if missing
    - Default `triggered_by` to "manual" in `app/jobs/r3x/run_workflow_job.rb` and always set builder
    - Update tests in `test/lib/r3x/*` to build contexts with a `triggered_by`
    - Purpose: make trigger source explicit and avoid nil-trigger bugs

--- app/jobs/r3x/run_workflow_job.rb
+++ app/jobs/r3x/run_workflow_job.rb
@@ -2,12 +2,12 @@ module R3x
   class RunWorkflowJob < ApplicationJob
     queue_as :default
 
-    def perform(workflow_key, triggered_by: nil)
+    def perform(workflow_key, triggered_by: "manual")
       R3x::WorkflowPackLoader.load!
       workflow_class = R3x::WorkflowRegistry.fetch(workflow_key)
 
       ctx = WorkflowContext.build do |builder|
-        builder.triggered_by = TriggeredBy.new(triggered_by) if triggered_by
+        builder.triggered_by = TriggeredBy.new(triggered_by)
         builder.with_solid_queue_task(workflow_key) if triggered_by == "schedule"
       end
 
