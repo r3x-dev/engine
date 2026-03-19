77620f28c6bfec1116e9885b9f528e019da53c29
zewelor <zewelor@gmail.com>
2026-03-19 10:22:01 +0000

    Allow empty Vault KV v2 data
    - Change check to require only `Hash` type (remove `present?`)
    - Accept empty secret hashes returned by Vault KV v2 responses
    - Prevent false negatives when `data.data` is an empty Hash

--- app/lib/r3x/client/hashi_corp_vault.rb
+++ app/lib/r3x/client/hashi_corp_vault.rb
@@ -21,7 +21,7 @@ module R3x
 
         secrets = response.body.is_a?(Hash) && response.body.dig("data", "data")
 
-        unless secrets.present? && secrets.is_a?(Hash)
+        unless secrets.is_a?(Hash)
           raise RuntimeError, "Vault response missing KV v2 data at data.data"
         end
 
