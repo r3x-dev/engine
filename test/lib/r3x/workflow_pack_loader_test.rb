f3647e4334e342750da8725abb4cee4390f96c1f
zewelor <zewelor@gmail.com>
2026-03-21 09:22:55 +0000

    Add workflow validator to enforce policy
    - Define strict policy constants for forbidden constants/modules/methods
    - Add AST-based `R3x::Workflow::Validator` to scan workflow files
    - Forbid direct `ENV`/`R3x::Env` access, shell exec, `eval`, `send`, etc.
    - Wire validator into `PackLoader.load!` to reject unsafe workflows at load
    - Add tests exercising validator and pack loader rejection behaviors

--- test/lib/r3x/workflow_pack_loader_test.rb
+++ test/lib/r3x/workflow_pack_loader_test.rb
@@ -37,5 +37,55 @@ module R3x
       assert_equal true, result["test"]
       assert_equal "Test workflow executed successfully", result["message"]
     end
+
+    test "rejects workflow that accesses ENV directly" do
+      Dir.mktmpdir do |dir|
+        wf_dir = File.join(dir, "env_violation_wf")
+        FileUtils.mkdir_p(wf_dir)
+        File.write(File.join(wf_dir, "workflow.rb"), <<~RUBY)
+          module Workflows
+            class EnvViolationWf < R3x::Workflow::Base
+              def run(ctx)
+                { secret: ENV["MY_SECRET"] }
+              end
+            end
+          end
+        RUBY
+
+        ENV["R3X_WORKFLOW_PATHS"] = wf_dir
+        assert_raises(R3x::Workflow::Validator::ForbiddenAccessError) do
+          R3x::Workflow::PackLoader.load!(force: true)
+        end
+      ensure
+        ENV["R3X_WORKFLOW_PATHS"] = @original_workflow_paths
+        R3x::Workflow::Registry.reset!
+        R3x::Workflow::PackLoader.load!(force: true)
+      end
+    end
+
+    test "rejects workflow that accesses R3x::Env directly" do
+      Dir.mktmpdir do |dir|
+        wf_dir = File.join(dir, "r3xenv_violation_wf")
+        FileUtils.mkdir_p(wf_dir)
+        File.write(File.join(wf_dir, "workflow.rb"), <<~RUBY)
+          module Workflows
+            class R3xenvViolationWf < R3x::Workflow::Base
+              def run(ctx)
+                { key: R3x::Env.fetch!("MY_KEY") }
+              end
+            end
+          end
+        RUBY
+
+        ENV["R3X_WORKFLOW_PATHS"] = wf_dir
+        assert_raises(R3x::Workflow::Validator::ForbiddenAccessError) do
+          R3x::Workflow::PackLoader.load!(force: true)
+        end
+      ensure
+        ENV["R3X_WORKFLOW_PATHS"] = @original_workflow_paths
+        R3x::Workflow::Registry.reset!
+        R3x::Workflow::PackLoader.load!(force: true)
+      end
+    end
   end
 end
