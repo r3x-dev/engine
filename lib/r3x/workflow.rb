e5503e700015fd182f01a770893db4ba2894515a
zewelor <zewelor@gmail.com>
2026-04-03 09:36:29 +0200

    Remove workflow capabilities DSL

--- lib/r3x/workflow.rb
+++ lib/r3x/workflow.rb
@@ -1,7 +1,5 @@
 module R3x
   module Workflow
-    KNOWN_CAPABILITIES = Set.new(%i[networking filesystem shell]).freeze
-
     module Dsl
       extend ActiveSupport::Concern
 
@@ -9,7 +7,6 @@ module R3x
         def inherited(subclass)
           super
           subclass._triggers = TriggerManager::Collection.new
-          subclass._capabilities = Set.new
         end
 
         def workflow_key
@@ -20,25 +17,6 @@ module R3x
           trigger_instance = Triggers.resolve(type).new(**options)
           trigger_instance.validate!(message_prefix: "Invalid trigger :#{type} for #{name}")
           _triggers.add(trigger_instance)
-        end
-
-        def uses(*capabilities)
-          incoming = Set.new(capabilities.flatten.compact.map(&:to_sym))
-          unknown = incoming - KNOWN_CAPABILITIES
-          raise ArgumentError, "Unknown capabilities: #{unknown.to_a.join(", ")}. Known: #{KNOWN_CAPABILITIES.to_a.join(", ")}" if unknown.any?
-
-          duplicates = incoming & _capabilities
-          raise ArgumentError, "Capability already declared: #{duplicates.to_a.join(", ")}" if duplicates.any?
-
-          _capabilities.merge(incoming)
-        end
-
-        def capabilities
-          _capabilities.dup
-        end
-
-        def uses?(capability)
-          _capabilities.include?(capability.to_sym)
         end
 
         # Returns all triggers, with a default Manual trigger if none declared
@@ -60,7 +38,6 @@ module R3x
         end
 
         attr_accessor :_triggers
-        attr_accessor :_capabilities
       end
     end
   end
