671d1cca0e46c75692ebb3ea51a5f510d4428d28
zewelor <zewelor@gmail.com>
2026-04-23 20:08:03 +0100

    chore: conditionally enable structured logging only when log provider is configured

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,12 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.clear_finished_jobs_after = 2.weeks
+
   config.solid_queue.logger = ActiveSupport::Logger.new(STDOUT).tap do |logger|
-    logger.formatter = config.log_formatter
+    if ENV["R3X_LOGS_PROVIDER"].present?
+      require_relative "../log_formatter"
+      logger.formatter = R3x::LogFormatter.new
+    end
   end
 
   unless R3x::RuntimeProfile.headless?
